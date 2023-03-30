70a6f3f8b6598c2209c480b4fecf13671daf8299
mouzougaai <57306138+mouzougaai@users.noreply.github.com>
2023-03-30 10:46:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,10 +6,10 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
+  USE_PASSAGE: 'push-deer'
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
-
+  
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
@@ -21,11 +21,11 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'PDU21377TyPDz3WyMe0aqc0ArjeofJzGBBSDPJDJ6',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '06-18',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
